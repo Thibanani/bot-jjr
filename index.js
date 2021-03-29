9d1c84dd02df0d125d9f9da0c7f39b3bce057da8
Thibanani <63426672+Thibanani@users.noreply.github.com>
2021-03-29 21:33:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ client.on('messageReactionAdd', async (reaction, user) => {
 	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
 	// The reaction is now also fully available and the properties will be reflected accurately:
 	console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+  if(reaction.message.channel != config.channel_role) return;
   
 
 
