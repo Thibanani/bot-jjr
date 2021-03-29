8531b3a6d321bf10289b10cef2cf87d2172c5df8
Thibanani <63426672+Thibanani@users.noreply.github.com>
2021-03-29 21:32:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ bot.on('ready', async() =>{
 
 
 /*------------------------------ Réaction émote au channel rôle ------------------------------*/
+client.on('messageReactionAdd', async (reaction, user) => {
+	// When we receive a reaction we check if the reaction is partial or not
+	if (reaction.partial) {// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Something went wrong when fetching the message: ', error);// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+	}
+	// Now the message has been cached and is fully available
+	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
+	// The reaction is now also fully available and the properties will be reflected accurately:
+	console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+  
+
+
+
+});
+/*
 bot.on('messageReactionAdd', (reaction, user) => {
 
   reaction.MessageReaction(bot,data,msg);
@@ -28,7 +48,7 @@ bot.on('messageReactionAdd', (reaction, user) => {
   const emoji = reactionRoleElem.emojis.find(emoji => emoji[prop] === reaction.emoji[prop])
   if (emoji) reaction.message.guild.member(user).roles.add(emoji.roles)
   else reaction.users.remove(user)*/
-})
+//})*/
 
 
 
