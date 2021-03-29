5f0c56712e67a6b0e8dafe56ea24f52e98dbc12a
Thibanani <63426672+Thibanani@users.noreply.github.com>
2021-03-29 21:48:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,34 @@
 const config = {
   prefix: "$",
   token: process.env.BOT_TOKEN,
-  channel_role: "jjr"
+  channel_role: "jjr",
 
+
+  role: {
+          channel: "yo",
+          role: "718970160647635056"
+      }
+
+
+/*
+  reactionRole : {
+      "721812281347932311": {
+          emojis: [{
+              id: "719563468008718348",
+              roles: "722404495371534358"
+          }]
+      },
+      "721812680763244596": {
+          removable: true,
+          emojis: [{
+              name: "💻",
+              roles: ["722088168458813584", "722088234963828747"]
+          }, {
+              name: "🎮",
+              roles: "722088255402672139"
+          }]
+      }
+    }*/
 };
 
 module.exports = config;
